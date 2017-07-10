6531f3b8c7cb79c9cca0a99510268729d9228ead
Scott <marden.scott@gmail.com>
2017-07-09 23:35:58 -0700

    Initial commit. Finished assignment: build a group chat that stores messages for users who log in afterwards, and uses sockets to display messages in real time on all connected clients as they are posted.

---
+++ server.js
@@ -0,0 +1,48 @@
+let express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+let bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+require("./server/config/routes.js")(app);
+
+let server = app.listen(3316, () => {
+	console.log("App listening on port 3316");
+});
+
+let io = require("socket.io").listen(server);
+
+var messages = [];
+
+function addToMessages(msg){
+	console.log(messages);
+	messages.push(msg);
+	console.log(messages);
+	return messages;
+}
+
+io.sockets.on("connection", (socket) => {
+	console.log(messages)
+	console.log("Socket, online!");
+	console.log(socket.id);
+	socket.emit("new_user", {messages: messages})
+
+	socket.on("new_message", (data) => {
+		console.log(data);
+		var msg = {'user': data.user, 'message': data.message};
+		addToMessages(msg);
+		let str = data.user + ": " + data.message;
+		console.log(str);
+		socket.broadcast.emit("add_message", {new_message: str});
+		return messages;
+	})
+});
